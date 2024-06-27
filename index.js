f7c67ace2a43512c12d7c8ba1388fe80e5eefe17
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-06-27 12:28:35 -0600

    basic js
    fixed a bug in the subtotal id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Create variables for the welcome message
 let greeting = 'Howdy ';
 let name = 'Molly ';
-let message = ', please check you order:';
+let message = ', please check your order:';
 // Concatenate the three variables above to create the welcome message
 let welcome = greeting + name + message;
 
