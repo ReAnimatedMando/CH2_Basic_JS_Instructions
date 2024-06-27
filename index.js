b855394942a5697b2ed624928b442e37b2778464
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-06-27 11:44:11 -0600

    basic js instr
    get element that has message of greeting for js code

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ let message = ', please check you order:';
 // Concatenate the three variables above to create the welcome message
 let welcome = greeting + name + message;
 
-//Create variables to hold details about the sign
+// Create variables to hold details about the sign
 let sign = 'Montague House';
 let tiles = sign.length;
 let subTotal = tiles * 5;
 let shipping = 7;
 let grandTotal = subTotal + shipping;
+
+// Get the element that has an id of greeting
+let el = document.getElementById('greeting');
+// Replace the content of that element with the personalized welcome message
+el.textContent = welcome;
 
