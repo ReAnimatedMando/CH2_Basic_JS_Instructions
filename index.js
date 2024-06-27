86bd41bb49c241026180d85671f47e03037c3a09
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2024-06-27 11:37:16 -0600

    basic js instr
    started .js for example page and inserted welcome message code

---
+++ index.js
@@ -0,0 +1,6 @@
+// Create variables for the welcome message
+let greeting = 'Howdy ';
+let name = 'Molly ';
+let message = ', please check you order:';
+// Concatenate the three variables above to create the welcome message
+let welcome = greeting + name + message;
